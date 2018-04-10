339c7e6ad38cf79af1c71ec88bb852f72e047d9a
heshun <2586625136@qq.com>
2018-04-10 19:09:02 +0800

    create project

---
+++ src/main/java/name/heshun/idm/IdmApplication.java
@@ -0,0 +1,12 @@
+package name.heshun.idm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdmApplication.class, args);
+	}
+}
